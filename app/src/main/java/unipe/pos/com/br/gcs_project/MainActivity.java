bbdb7b47f44461b984d63c5e23606c2da2969840
Vinicius Viana <vvv1010@hotmail.com>
2018-03-04 00:41:13 -0300

    Implementação da informação NOME ao calcular

--- app/src/main/java/unipe/pos/com/br/gcs_project/MainActivity.java
+++ app/src/main/java/unipe/pos/com/br/gcs_project/MainActivity.java
@@ -1,7 +1,10 @@
 package unipe.pos.com.br.gcs_project;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,11 +13,12 @@ import android.widget.ViewAnimator;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText pesoT, alturaT;
+    private EditText pesoT, alturaT, nomeT;
     private TextView resultadoT;
     private Button btnCalcular;
 
     private Double peso, altura, resultado;
+    private String nome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +35,39 @@ public class MainActivity extends AppCompatActivity {
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                showDialog();
+            }
+        });
+    }
+
+    protected void showDialog(){
+        LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+        View view = layoutInflater.inflate(R.layout.input_dialog, null);
+        AlertDialog.Builder aBuilder = new AlertDialog.Builder(MainActivity.this);
+        aBuilder.setView(view);
+
+        nomeT = (EditText) view.findViewById(R.id.NameId);
+
+        aBuilder.setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
                 peso = Double.parseDouble(pesoT.getText().toString());
                 altura = Double.parseDouble(alturaT.getText().toString());
+                nome = nomeT.getText().toString();
 
                 resultado = (peso)/(altura*altura);
 
-                resultadoT.setText(resultado.toString());
+                resultadoT.setText(nome + " seu IMC é: " + resultado.toString());
             }
-        });
+        }).setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
 
-
+        AlertDialog alert = aBuilder.create();
+        alert.show();
     }
+
 }
